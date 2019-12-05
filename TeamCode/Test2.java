40e3c3044dae66bd46b62292c293feb34f260e29
Shashankg123853 <58535175+Shashankg123853@users.noreply.github.com>
2019-12-04 18:44:53 -0600

    Update Test2.java

--- TeamCode/Test2.java
+++ TeamCode/Test2.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 @Autonomous(name = "Test2")
 Public class Test2 extends LinearOpMode {
 private DcMotor leftDrive, rightDrive;
-double power = 0.5;
+
 
 @Override
 public void runOpMode()
@@ -12,9 +12,18 @@ leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
 rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
 waitForStart();
- leftDrive.setPower(power);
- rigthDrive.setPower(power);
- sleep(1000);
+drive(motorPower(0.4, 1000);
+ drive(motorPower(0.3,300);
+ drive(motorPower(-0.7,100);
 
 }
+ public double motorPower (double speed) {
+  double power = 1*speed;
+  return power;
+ }
+ public void drive (double power, double time) {
+ leftDrive.setPower(.power);
+  rightDrive.setPower(-power);
+  sleep(1000);
+ }
 }
